78e29095f90613ac50820393c0c91d770816a036
Jhazeel Hernandez <jhazeelhdz@gmail.com>
2024-06-28 16:42:46 -0600

    Add n event listener to all the  buttons

---
+++ script.js
@@ -0,0 +1,12 @@
+let buttons = document.querySelector(".buttons");
+let output = document.querySelector(".result");
+
+buttons.addEventListener("click", event => {
+    let targetId = event.target.id;
+    let targetClass = event.target.className;
+    
+
+
+
+
+});
