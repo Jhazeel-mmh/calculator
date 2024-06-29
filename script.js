99b5e777af770b949b3492cc6d16f429f80112a7
Jhazeel Hernandez <jhazeelhdz@gmail.com>
2024-06-29 11:02:08 -0600

    Add the operator function

--- script.js
+++ script.js
@@ -1,12 +1,62 @@
-let buttons = document.querySelector(".buttons");
+let btns = document.querySelector(".buttons");
 let output = document.querySelector(".result");
-
-buttons.addEventListener("click", event => {
-    let targetId = event.target.id;
-    let targetClass = event.target.className;
-    
+let num1 = 0, num2 = 0, result = 0, operatorOp;
 
 
+btns.addEventListener("click", event => {
+    let target = event.target;
+    let targetId = target.id;
+    let targetClass = target.className;
 
+
+    if (targetClass === "numbers"){
+        output.textContent += target.value;
+    }
+
+    if (targetClass === "operators"){
+
+        if (targetId === "equal"){
+            if (!(num1 || num2)){
+                return error();
+            }
+            if (!(result)){
+                result = operator(num1, num2, operatorOp);
+            }
+            output.textContent = result;
+        }
+
+        if (!(num1)){
+            num1 = output.textContent;
+        }
+
+        if (!(num2)){
+            num2 = output.textContent;
+        }
+        
+        operatorOp = target.value;
+        output.textContent = "";
+    }
 
 });
+
+
+function operator(num1, num2, operator){
+    let a = Number(num1);
+    let b = Number(num2);
+    switch (operator){
+        case "+":
+            return a + b;
+            break;
+        case "-":
+            return a - b;
+            break;
+        case "/":
+            return a / b;
+            break;
+        case "*":
+            return a * b;
+            break;
+        default:
+            return error(); 
+    }
+};
